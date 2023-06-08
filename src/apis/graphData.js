e6de44d0fe6fcfe527cfc7802b2d3ce7174b0c15
Josip <josip.relja5@gmail.com>
2023-06-08 22:48:37 +0200

    a

--- src/apis/graphData.js
+++ src/apis/graphData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "production"
     ? "oglasi"
-    : "postgres://default:gXh7vYRPn1Cz@ep-royal-dust-927701-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb";
+    : "http://localhost:4000/oglasi";
 
 export default axios.create({
   baseURL,
