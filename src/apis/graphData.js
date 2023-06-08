b29d08ce69dc50946ff52423dfd704bc75ff956a
Josip <josip.relja5@gmail.com>
2023-06-09 00:08:06 +0200

    6545645

--- src/apis/graphData.js
+++ src/apis/graphData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "oglasi"
+    ? "/"
     : "https://diplomski-api.vercel.app/api/v1/oglasi";
 
 export default axios.create({
