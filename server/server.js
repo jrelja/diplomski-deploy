415d0807e34df2c9030fb5501444dac8cd014a85
Josip <josip.relja5@gmail.com>
2023-06-08 22:48:45 +0200

    aaaa

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get("/oglasi/:id", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
 });
